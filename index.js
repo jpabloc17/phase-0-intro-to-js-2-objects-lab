aed8ac0c9464a12a72773b0d7cb64ee8b7719109
Pablo Castro <pablocas-tro1@outlook.com>
2023-03-03 17:45:48 -0500

    Completed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Pablo",
+    streetAddress: "35 WaterGarde"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,[key]:value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    return {...delete employee.key};
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
